a9ae0b9a147837b45f277c71b58c945964290325
j540 <james@tokyowheel.com>
2014-12-20 08:48:17 +0700

    .allow_ssl_in_production = false

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module Tkwl
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    config.allow_ssl_in_production = false
   end
 end

77d01eb48b7e046bf0357619592920eb2b663cb0
j540 <james@tokyowheel.com>
2015-01-07 17:56:23 +0700

    wombat order model + routing quick fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_locale
+ 
+ def default_url_options(options={})
+    { locale: I18n.locale }
+  end
 
- private
-     def set_locale
-       I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
-       session[:locale] = I18n.locale
-     end
-
-    def default_url_options(options={})
-     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-      { locale: I18n.locale }
+private
+    def set_locale
+      I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+      session[:locale] = I18n.locale
     end
 end

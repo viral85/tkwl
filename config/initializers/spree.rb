c7d2e938d2d64f79b28e07ca165e7129eb7bb832
j540 <james@tokyowheel.com>
2015-01-07 17:25:43 +0700

    change to environmental variables

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -16,15 +16,15 @@ Spree.config do |config|
   attachment_config = {
 
     s3_credentials: {
-      access_key_id:     "AKIAJR74OVITX3RKCODA",
-      secret_access_key: "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",
-      bucket:            "tkwl"
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      bucket:            ENV['S3_BUCKET_NAME']
     },
     s3_host_alias: 'd1fw984ffi6nce.cloudfront.net',
     storage:        :s3,
     s3_headers:     { "Cache-Control" => "max-age=31557600" },
     s3_protocol:    "https",
-    bucket:         "tkwl",
+    bucket:         ENV['S3_BUCKET_NAME'],
     url:            ":s3_alias_url",
 
     styles: {

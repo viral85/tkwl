055715b27310b57dcdd4835f8c50ab7dcd0f47b6
suchith.j <suchith.j@fidenz.com>
2015-01-22 15:56:55 +0530

    deployment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -29,6 +29,7 @@ Rails.application.config.assets.precompile += %w( frontend/out-of-stock.png )
 Rails.application.config.assets.precompile += %w( logo/spree_50.png )
 Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/images/preloader.gif )
 Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/images/gal_add.png )
+Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/images/gal_more.gif )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.

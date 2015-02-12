d5faccfb7e33d23abf93eef7797b9e1eadc5041a
j540 <james@tokyowheel.com>
2015-02-12 17:06:48 +0700

    enable blog entries to use ERB code and add blog views to main app

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   
  
       mount Spree::Core::Engine, :at => '/'
+      
+      
+      #Legacy URL Redirects
+      get 'my-url', to: redirect('/products/epic-38-carbon-clincher-wheelset')
  
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

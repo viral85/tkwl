824aba5769fa6d6c5e4b8b72ca19a27ee6b282cb
j540 <james@tokyowheel.com>
2015-02-20 10:06:40 +0700

    set asset precompie to false and variant tracking to false

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,8 @@
 Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+  config.track_inventory_levels = false
+  config.assets.initialize_on_precompile = false 
   config.allow_ssl_in_production = false
   
    if Rails.env.production? then
